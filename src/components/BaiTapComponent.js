86fd0f6b2538886fb796745a2c18850da8c2d21e
Ha-Hoang <hoangthuyhaiha@gmail.com>
2021-04-08 13:11:44 +0700

    baiTapComponent

---
+++ src/components/BaiTapComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from "./Navbar";
+import Intro from "./Intro";
+import Carousel from "./Carousel";
+import Footer from "./Footer";
+
+function BaiTapComponent() {
+    return (
+        <div>
+            <Navbar />
+            <Carousel />
+            <Intro />
+            <Footer />
+        </div>
+    );
+}
+
+export default BaiTapComponent;
+
+
+
+
+
